31e38f9bb7318a6ff04c9dbb9f039430649d0c75
sbrb <info.sbriju@gmail.com>
2023-03-10 14:15:58 +0530

    h

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ io.on("connection", socket => {
     socket.on("send-changes", delta => {
       socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
-    console.log(socket.on("send-changes", delta => {
-      socket.broadcast.to(documentId).emit("receive-changes", delta)
-    }))
+
     socket.on("save-document", async data => {
       await Document.findByIdAndUpdate(documentId, { data })
     })
@@ -44,6 +42,6 @@ async function findOrCreateDocument(id) {
   if (id == null) return;
 
   const document = await Document.findById(id)
-  if (document) return document;
+  if (document) return document
   return await Document.create({ _id: id, data: defaultValue })
 }
